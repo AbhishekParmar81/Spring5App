4c2ddef789390439f988f17371fa90e6d164fb01
Abhishek Parmar <aparmar@APARMAR-2555.local>
2019-08-14 11:52:27 -0700

    first commit

---
+++ src/main/java/net/unicon/Spring5App/Spring5AppApplication.java
@@ -0,0 +1,13 @@
+package net.unicon.Spring5App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5AppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/unicon/Spring5App/Spring5AppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.unicon.Spring5App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
